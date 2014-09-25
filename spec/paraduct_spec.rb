7dce0edc3d00948fceb1cb674ec0d7df2a8cd3c5
sue445 <sue445@sue445.net>
2014-09-25 22:58:00 +0900

    Test: red -> green

--- spec/paraduct_spec.rb
+++ spec/paraduct_spec.rb
@@ -1,11 +1,5 @@
-require 'spec_helper'
-
 describe Paraduct do
   it 'has a version number' do
     expect(Paraduct::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
