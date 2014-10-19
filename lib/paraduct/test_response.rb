ef23e01974febf117eafb3edb445b7fdfb79c19d
sue445 <sue445@sue445.net>
2014-10-20 00:41:46 +0900

    Impl detail_message

--- lib/paraduct/test_response.rb
+++ lib/paraduct/test_response.rb
@@ -15,5 +15,28 @@ module Paraduct
     def failure?
       !successful?
     end
+
+    def detail_message
+      all_count = @jobs.count
+      successful_count = @jobs.select{ |result| result[:successful] }.count
+      failure_count = all_count - successful_count
+
+      message = ""
+
+      if failure_count > 0
+        message << "Failures:\n\n"
+        @jobs.select{ |result| !result[:successful] }.each_with_index do |result, i|
+          formatted_params = result[:params].inject([]) { |array, (key, value)|
+            array << "#{key}=#{value}"
+            array
+          }.join(", ")
+          message << "  #{i+1}) #{formatted_params}\n"
+        end
+        message << "\n"
+      end
+      message << "#{all_count} jobs, #{failure_count} failures, #{successful_count} passed\n"
+
+      message
+    end
   end
 end
