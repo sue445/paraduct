10d113da1179b33c13dd40256b285551f107026e
sue445 <sue445@sue445.net>
2014-10-30 01:31:46 +0900

    Impl: dry-run option

--- spec/paraduct/cli_spec.rb
+++ spec/paraduct/cli_spec.rb
@@ -19,52 +19,63 @@ describe Paraduct::CLI do
     include_context :stub_configuration
     include_context :within_temp_dir
 
-    context "successful test" do
-      let(:config_data) do
-        {
-          script: "./script/build_success.sh",
-          work_dir: "tmp/paraduct_workspace",
-          variables: {
-            ruby:     ["1.9.3", "2.0.0"],
-            database: ["mysql", "postgresql"],
-          },
-        }
+    context "with no option" do
+      context "successful test" do
+        let(:config_data) do
+          {
+            script: "./script/build_success.sh",
+            work_dir: "tmp/paraduct_workspace",
+            variables: {
+              ruby:     ["1.9.3", "2.0.0"],
+              database: ["mysql", "postgresql"],
+            },
+          }
+        end
+
+        let(:script){ "./script/build_success.sh" }
+        let(:product_variables) do
+          [
+            { "ruby" => "1.9.3", "database" => "mysql"      },
+            { "ruby" => "1.9.3", "database" => "postgresql" },
+            { "ruby" => "2.0.0", "database" => "mysql"      },
+            { "ruby" => "2.0.0", "database" => "postgresql" },
+          ]
+        end
+
+        let(:test_response) do
+          test_response = Paraduct::TestResponse.new
+          test_response.jobs_push(successful: true)
+          test_response
+        end
+
+        it "should call perform_all" do
+          expect(Paraduct::ParallelRunner).to receive(:perform_all).with(script, product_variables){ test_response }
+          subject
+        end
       end
 
-      let(:script){ "./script/build_success.sh" }
-      let(:product_variables) do
-        [
-          { "ruby" => "1.9.3", "database" => "mysql"      },
-          { "ruby" => "1.9.3", "database" => "postgresql" },
-          { "ruby" => "2.0.0", "database" => "mysql"      },
-          { "ruby" => "2.0.0", "database" => "postgresql" },
-        ]
-      end
+      context "failure test" do
+        let(:config_data) do
+          {
+            script: %q(exit ${STATUS}),
+            work_dir: "tmp/paraduct_workspace",
+            variables: {
+              status: [0, 1],
+            },
+          }
+        end
 
-      let(:test_response) do
-        test_response = Paraduct::TestResponse.new
-        test_response.jobs_push(successful: true)
-        test_response
-      end
-
-      it "should call perform_all" do
-        expect(Paraduct::ParallelRunner).to receive(:perform_all).with(script, product_variables){ test_response }
-        subject
+        it { expect{ subject }.to raise_error Paraduct::Errors::TestFailureError }
       end
     end
 
-    context "failure test" do
-      let(:config_data) do
-        {
-          script: %q(exit ${STATUS}),
-          work_dir: "tmp/paraduct_workspace",
-          variables: {
-            status: [0, 1],
-          },
-        }
-      end
+    context "with --dry-run" do
+      let(:args){ [command, "--dry-run"] }
 
-      it { expect{ subject }.to raise_error Paraduct::Errors::TestFailureError }
+      it "should not call perform_all" do
+        expect(Paraduct::ParallelRunner).not_to receive(:perform_all)
+        subject
+      end
     end
   end
 
