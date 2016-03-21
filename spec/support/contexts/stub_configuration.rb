27fafdbf417216e9f7486b65e1c1a5a88000f0c5
sue445 <sue445@sue445.net>
2016-03-21 12:50:26 +0900

    Tweak stub

--- spec/support/contexts/stub_configuration.rb
+++ spec/support/contexts/stub_configuration.rb
@@ -8,8 +8,8 @@ shared_context :stub_configuration do
       script: %q(echo "NAME1=${NAME1}, NAME2=${NAME2}"),
       work_dir: "tmp/paraduct_workspace",
       variables: {
-        name1: ["value1a", "value1b"],
-        name2: ["value2a", "value2b"],
+        "NAME1" => ["value1a", "value1b"],
+        "NAME2" => ["value2a", "value2b"],
       },
     }
   end
