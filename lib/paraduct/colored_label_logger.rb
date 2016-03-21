b81fd46d8672fe7b893c43cbc97aa0ea8dbd7dde
sue445 <sue445@sue445.net>
2016-03-21 22:24:48 +0900

    Use formatter

--- lib/paraduct/colored_label_logger.rb
+++ lib/paraduct/colored_label_logger.rb
@@ -3,19 +3,20 @@ module Paraduct
     def initialize(label_name, logdev = STDOUT)
       super(logdev)
       color = Paraduct::ColoredLabelLogger.next_color
-      @label = "[#{label_name.to_s.colorize(color)}]"
-      @formatter = ActiveSupport::Logger::SimpleFormatter.new
+      # @label = "[#{label_name.to_s.colorize(color)}]"
+      # @formatter = ActiveSupport::Logger::SimpleFormatter.new
+      @formatter = Formatter.new(label_name, color)
     end
 
-    SEVERITIES = [:debug, :info, :warn, :error, :fatal]
-    SEVERITIES.each do |severity|
-      define_method "#{severity}_with_label" do |message|
-        message.each_line do |line|
-          send "#{severity}_without_label", "#{@label} #{line.strip}" unless line.blank?
-        end
-      end
-      alias_method_chain severity, :label
-    end
+    # SEVERITIES = [:debug, :info, :warn, :error, :fatal]
+    # SEVERITIES.each do |severity|
+    #   define_method "#{severity}_with_label" do |message|
+    #     message.each_line do |line|
+    #       send "#{severity}_without_label", "#{@label} #{line.strip}" unless line.blank?
+    #     end
+    #   end
+    #   alias_method_chain severity, :label
+    # end
 
     COLORS = [
       :cyan,
@@ -36,6 +37,22 @@ module Paraduct
       @color_index ||= -1
       @color_index = (@color_index + 1) % COLORS.length
       COLORS[@color_index]
+    end
+
+    class Formatter
+      def initialize(label_name, color)
+        @label = "[#{label_name.to_s.colorize(color)}]"
+      end
+
+      def call(severity, datetime, progname, message)
+        return "" if message.blank?
+
+        content = ""
+        message.each_line do |line|
+          content << "#{@label} #{line.strip}\n"
+        end
+        content
+      end
     end
   end
 end
